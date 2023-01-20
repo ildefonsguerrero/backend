e8a9505366e7c2189b3dd8d5a1d0df11a6715242
unknown <serduero@gmail.com>
2023-01-20 19:22:08 +0100

    nuevo origen

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,13 @@ const router = Router();
 
 import control from "../controllers/control.js";
 import sendMail from "../controllers/sendMail.js";
+import sendMail2 from "../controllers/sendMail2.js";
 
 // Control de acceso
 router.all("*", control);
 
 // Enviar correo
 router.post("/sendMail/:parms", sendMail);
+router.post("/sendMail2/:parms", sendMail2);
 
 export default router;
